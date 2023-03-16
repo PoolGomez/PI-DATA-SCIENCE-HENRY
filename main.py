09b86a229f535d4c782f471bac9b9a2e6fd6814b
paul479 <pgomez4790@gmail.com>
2023-03-16 12:05:54 -0500

    second commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from pandas import pd
+##from numpy import np
+
+app = FastAPI()
+    
+#http://127.0.0.1:8000
+@app.get("/")
+def index():
+    return {"mensaje":"Hola,pythones"}
